76ea926618c127d797bd9feb64f12ffe1496a5f9
ahmadmm01 <skyahmad74@gmail.com>
2022-03-27 20:12:42 +0700

    Create activity activity_task

---
+++ app/src/main/java/com/pam/exercise1_140_a/activity_task.java
@@ -0,0 +1,14 @@
+package com.pam.exercise1_140_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
